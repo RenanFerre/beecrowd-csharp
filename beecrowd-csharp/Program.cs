49b0ee675f07f1b911fe86a781d21a3663503081
RenanFerre <renanferreira9691@gmail.com>
2026-09-08 15:12:24 -0300

    feat: resolve problema 1001 - Extremamente Basico

--- beecrowd-csharp/Program.cs
+++ beecrowd-csharp/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int x = a + b;
+        Console.WriteLine($"X = {x}");
+    }
+}
