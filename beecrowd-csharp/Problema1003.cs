0e98cb1c2cb97d58dc4a7dd95a907e8275a0420c
RenanFerre <renanferreira9691@gmail.com>
2026-09-08 15:42:18 -0300

    feat: solucao problema 1003 - Soma de A e B.

---
+++ beecrowd-csharp/Problema1003.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Beecrowd;
+
+class Problema1003
+{
+    static void Main(string[] args)
+    {
+        int A, B, soma;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+
+        soma = A + B;
+
+        Console.WriteLine("SOMA = " + soma);
+    }
+}
