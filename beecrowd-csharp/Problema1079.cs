a8ba33531b7ed2f569aa836b1af2f4b5651e6630
RenanFerre <renanferreira9691@gmail.com>
2026-09-09 09:15:25 -0300

    feat: solucao problema 1079 - Média.

---
+++ beecrowd-csharp/Problema1079.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd;
+
+class Problema1079
+{
+    static void Main(string[] args)
+    {
+        int N;
+        double n1, n2, n3, media;
+
+        N = int.Parse(Console.ReadLine());
+
+
+        for (int i = 0; i < N; i++)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            n1 = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            n2 = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            n3 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            media = ((n1 * 2) + (n2 * 3) + (n3 * 5)) / 10.0;
+            Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+
+        Console.ReadKey();
+    }
+}
